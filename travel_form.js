3e96f88fb7565f85592a1f2c1f648868b136e9d1
Tshepiso <artsethmedia01@gmail.com>
2026-03-21 23:56:22 +0200

    Update travel_form.js

--- travel_form.js
+++ travel_form.js
@@ -12,14 +12,5 @@
         });
         
         // FAQ toggle functionality
-        document.querySelectorAll('.faq-item h3').forEach(question => {
-            question.addEventListener('click', () => {
-                const answer = question.nextElementSibling;
-                const icon = question.querySelector('i');
-                
-                answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-                icon.classList.toggle('fa-chevron-down');
-                icon.classList.toggle('fa-chevron-up');
-            });
-        });
+
 
