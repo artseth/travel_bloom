a02ac3e4106335544c9c6c6a544ecbfde8f3d439
Tshepiso <artsethmedia01@gmail.com>
2026-03-27 21:55:00 +0200

    Update tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,15 +1,4 @@
- document.getElementById('contactForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            
-            // Simple validation
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            
-            if (name && email) {
-                alert('Thank you for your message! We will get back to you within 24 hours.');
-                this.reset();
-            }
-        });
+
         
         // FAQ toggle functionality
         document.querySelectorAll('.faq-item h3').forEach(question => {
